dccea966a278f1797e62c2693705a2092d32f81e
Haseeb Siddiqi <Haseebs727@gmail.com>
2023-01-15 12:51:31 -0500

    Changed contact box

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -53,9 +53,9 @@ const Cta = ({
               label="Subscribe"
               labelHidden
               hasIcon="right"
-              placeholder="Your email"
+              placeholder="Write your email"
             >
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+              <svg width="20" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
                   fill="#376DF9"
